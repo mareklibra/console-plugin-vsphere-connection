b3f375ee9fdf810bdb3bea79740e190d7c79344b
Marek Libra <marek.libra@gmail.com>
2022-08-04 14:26:45 +0200

    Refactor getVSphereHealth

--- src/components/ClusterOverview/VSphereStatus.tsx
+++ src/components/ClusterOverview/VSphereStatus.tsx
@@ -2,99 +2,24 @@ import * as React from 'react';
 import { StackItem, Stack } from '@patternfly/react-core';
 import {
   HealthState,
-  PrometheusResult,
-  PrometheusValue,
   StatusPopupItem,
   StatusPopupSection,
 } from '@openshift-console/dynamic-plugin-sdk';
 import {
   PrometheusHealthPopupProps,
   PrometheusHealthHandler,
-  SubsystemHealth,
 } from '@openshift-console/dynamic-plugin-sdk/lib/extensions/dashboard-types';
 import { Link } from 'react-router-dom';
+
 import { useTranslation } from '../../i18n';
 import { VSphereConnection } from '../VSphereConnection';
 import { ConfigMap } from '../../resources';
+import { getVSphereHealth } from '../getVSphereHealth';
 
 import './VSphereStatus.css';
-import { TFunction } from 'react-i18next';
 
 // https://issues.redhat.com/browse/MGMT-9085
 // https://access.redhat.com/solutions/6677901
-
-const getPrometheusMetricValue = (
-  prometheusResult: PrometheusResult[],
-  reason: string,
-): PrometheusValue | undefined => prometheusResult.find((r) => r.metric.reason === reason)?.value;
-
-const getVSphereHealth = (
-  t: TFunction = (s: string) => s,
-  responses: PrometheusHealthPopupProps['responses'],
-  configMapResult: PrometheusHealthPopupProps['k8sResult'],
-): SubsystemHealth => {
-  if (!configMapResult) {
-    return { state: HealthState.LOADING };
-  }
-
-  if (configMapResult.loadError) {
-    return {
-      state: HealthState.WARNING,
-      message: t('Failed to load vSphere connection config map.'),
-    };
-  }
-
-  if (!configMapResult.loaded) {
-    return { state: HealthState.LOADING };
-  }
-
-  // by vSphere Problem Detector
-  if (responses.length < 1) {
-    return { state: HealthState.LOADING };
-  }
-
-  if (responses.find((r) => r.error)) {
-    console.error('Prometheus query error: ', responses);
-    return { state: HealthState.ERROR, message: t('Prometheus query failed.') };
-  }
-
-  if (!responses[0].response?.status) {
-    return { state: HealthState.LOADING };
-  }
-
-  const prometheusResult = responses[0].response?.data?.result;
-  if (responses[0].response.status !== 'success' || !prometheusResult) {
-    console.error('Prometheus query error: ', responses);
-    return { state: HealthState.ERROR, message: t('Prometheus query failed.') };
-  }
-
-  if (getPrometheusMetricValue(prometheusResult, 'InvalidCredentials')?.[0]) {
-    console.error(
-      'Prometheus query - invalid credentials: ',
-      getPrometheusMetricValue(prometheusResult, 'InvalidCredentials'),
-    );
-    return { state: HealthState.WARNING, message: t('Invalid credentials') };
-  }
-
-  if (getPrometheusMetricValue(prometheusResult, 'SyncError')?.[0]) {
-    console.error(
-      'Prometheus query - synchronization failed: ',
-      getPrometheusMetricValue(prometheusResult, t('SyncError')),
-    );
-    return { state: HealthState.WARNING, message: 'Synchronization failed' };
-  }
-
-  const anyFailingMetric = prometheusResult.find((r) => r.value?.[0]);
-  if (anyFailingMetric) {
-    console.error('Prometheus query - a failing metric found: ', anyFailingMetric);
-    return {
-      state: HealthState.WARNING,
-      message: t('Failing {{reason}}', { reason: anyFailingMetric.metric.reason }),
-    };
-  }
-
-  return { state: HealthState.OK };
-};
 
 const VSphereStatus: React.FC<PrometheusHealthPopupProps & { hide: () => void }> = ({
   hide,
@@ -135,8 +60,8 @@ const VSphereStatus: React.FC<PrometheusHealthPopupProps & { hide: () => void }>
   );
 };
 
-export default VSphereStatus;
-
 export const healthHandler: PrometheusHealthHandler = (responses, _skip, additionalResource) => {
   return { state: getVSphereHealth(undefined, responses, additionalResource).state };
 };
+
+export default VSphereStatus;
